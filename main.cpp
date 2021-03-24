57bcc333c303b2830b1c3ad597b504f8ea948e0f
Marius Seufzer <marius.seufzer@icloud.com>
2021-03-24 12:49:20 +0100

    Update CMake

--- main.cpp
+++ main.cpp
@@ -4,26 +4,20 @@
 #include "libs/IT8951/e-Paper/EPD_IT8951.h"
 
 IT8951_Dev_Info deviceInfo;
-uint16_t panelWidth;
-uint16_t panelHeight;
 uint32_t initTargetMemoryAddress;
-char* lutVersion;
 uint8_t a2Mode;
 
 int main() {
-    if(DEV_Module_Init()!=0){
+    if(DEV_Module_Init()!=0) {
         DEV_Module_Exit();
         throw std::runtime_error("Could not initialize bcm2835.");
     }
 
     const uint16_t vcom = 1.48 * 1000;
     deviceInfo = EPD_IT8951_Init(vcom);
-    panelWidth = deviceInfo.Panel_W;
-    panelHeight = deviceInfo.Panel_H;
     initTargetMemoryAddress = deviceInfo.Memory_Addr_L | (deviceInfo.Memory_Addr_H << 16);
-    lutVersion = (char*)deviceInfo.LUT_Version;
 
-    if (strcmp(lutVersion, "M841_TFA5210") == 0 ){
+    if (strcmp((char*)deviceInfo.LUT_Version, "M841_TFA5210") == 0 ){
         //10.3inch e-Paper HAT(1872,1404)
         a2Mode = 6;
     } else {
