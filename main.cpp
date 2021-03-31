512233e9bad100d3446397d6dcec60b207b207c9
Marius Seufzer <marius.seufzer@icloud.com>
2021-03-31 13:36:01 +0200

    Remove global variables

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@ extern "C" {
 #include "libs/IT8951/e-Paper/EPD_IT8951.h"
 }
 
-IT8951_Dev_Info deviceInfo;
-uint32_t initTargetMemoryAddress;
-uint8_t a2Mode;
-
 int main() {
     if(DEV_Module_Init()!=0) {
         DEV_Module_Exit();
@@ -16,17 +12,8 @@ int main() {
     }
 
     const uint16_t vcom = 1.48 * 1000;
-    deviceInfo = EPD_IT8951_Init(vcom);
-    initTargetMemoryAddress = deviceInfo.Memory_Addr_L | (deviceInfo.Memory_Addr_H << 16);
-
-    if (strcmp((char*)deviceInfo.LUT_Version, "M841_TFA5210") == 0 ){
-        //10.3inch e-Paper HAT(1872,1404)
-        a2Mode = 6;
-    } else {
-        DEV_Module_Exit();
-        EPD_IT8951_Sleep();
-        throw std::runtime_error("Unsupported E Paper display.");
-    }
+    IT8951_Dev_Info deviceInfo = EPD_IT8951_Init(vcom);
+    uint32_t initTargetMemoryAddress = deviceInfo.Memory_Addr_L | (deviceInfo.Memory_Addr_H << 16);
 
     EPD_IT8951_Init_Refresh(deviceInfo, initTargetMemoryAddress);
     EPD_IT8951_Sleep();
